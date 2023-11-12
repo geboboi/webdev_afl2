5b3d06261fd809ded2f80509ed0db06dc60edc91
WillasT <wrorrong@student.ciputra.ac.id>
2023-11-12 19:08:12 +0700

    final final fixxxx

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -26,7 +26,7 @@ class ProductSeeder extends Seeder
             'price' => "12000",
             'description' => "Lumpia adalah salah satu makanan ringan yang terkenal di berbagai negara Asia, termasuk Indonesia, Filipina, dan Tiongkok. Di Indonesia, lumpia sering dianggap sebagai salah satu camilan yang populer. Ada dua jenis lumpia yang umumnya dikenal, yaitu lumpia basah dan lumpia goreng.",
             'image' => "assets/img/product/4.png",
-            'promo_id' => null,
+            'promo_id' => 2,
         ]);
 
         DB::table('products')->insert([
@@ -34,14 +34,14 @@ class ProductSeeder extends Seeder
             'price' => "12000",
             'description' => "Kroket Ragout adalah hidangan yang terdiri dari kroket yang diisi dengan isian berupa ragout. Ini adalah makanan yang umumnya dianggap sebagai camilan atau hidangan pembuka, terutama di beberapa negara Eropa. Kroket adalah bola atau tabung berbentuk silinder yang terbuat dari campuran kentang yang dihaluskan atau puree, terkadang dicampur dengan mentega, susu, dan bumbu-bumbu lainnya. Ragout adalah hidangan krim atau saus yang biasanya terbuat dari daging cincang, sayuran, dan bumbu.",
             'image' => "assets/img/product/5.png",
-            'promo_id' => 3,
+            'promo_id' => null,
         ]);
         DB::table('products')->insert([
             'name' => "Pastel Tutup Besar",
             'price' => "250000",
             'description' => "Pastel panggang adalah hidangan yang umumnya terdiri dari kulit yang tipis dan renyah yang membalut berbagai macam isian. Proses panggang memberikan pastel ini tekstur luar yang garing sementara memastikan bahwa isian di dalamnya matang. Pastel ini memiliki tempat yang besar sehingga bisa dinikmati oleh banyak orang",
             'image' => "assets/img/product/2.png",
-            'promo_id' => 4,
+            'promo_id' => 3,
         ]);
 
         DB::table('products')->insert([
